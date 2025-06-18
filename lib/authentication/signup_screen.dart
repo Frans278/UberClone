251a33593caff44f84748d4d0038fe179fbedcf8
Frans278 <franssegwale32@gmail.com>
2025-06-19 00:32:26 +0200

    User App

---
+++ lib/authentication/signup_screen.dart
@@ -0,0 +1,234 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:users_app/authentication/login_screen.dart';
+import 'package:users_app/methods/common_methods.dart';
+import 'package:users_app/pages/home_page.dart';
+import 'package:users_app/widgets/loading_dialog.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+
+class _SignupScreenState extends State<SignupScreen>
+{
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController userPhoneTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  CommonMethods cMethods = CommonMethods();
+
+
+  checkIfNetworkIsAvailable()
+  {
+    cMethods.checkConnectivity(context);
+
+    signUpFormValidation();
+  }
+
+  signUpFormValidation()
+  {
+    if(userNameTextEditingController.text.trim().length < 3)
+    {
+      cMethods.displaySnackBar("Your user name must be atleast 4 or more characters.", context);
+    }
+    else if (userPhoneTextEditingController.text.trim().length < 9)
+    {
+      cMethods.displaySnackBar("Please enter a valid phone number.", context);
+    }
+    else if (!emailTextEditingController.text.contains("@"))
+    {
+      cMethods.displaySnackBar("Please enter a valid email.", context);
+    }
+    else if (passwordTextEditingController.text.trim().length < 5)
+    {
+      cMethods.displaySnackBar("Your password must be 6 or more characters.", context);
+    }
+    else
+    {
+      registerNewUser();
+    }
+  }
+
+  registerNewUser() async
+  {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => LoadingDialog(messageText: "Registering your account..."),
+    );
+
+    final User? userFirebase = (
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailTextEditingController.text.trim(),
+        password: passwordTextEditingController.text.trim(),
+      ).catchError((errorMsg)
+      {
+        Navigator.pop(context);
+        cMethods.displaySnackBar(errorMsg.toString(), context);
+      })
+    ).user;
+
+    if(!context.mounted) return;
+    Navigator.pop(context);
+
+    DatabaseReference usersRef = FirebaseDatabase.instance.ref().child("users").child(userFirebase!.uid);
+    Map userDataMap =
+    {
+      "name": userNameTextEditingController.text.trim(),
+      "email": emailTextEditingController.text.trim(),
+      "phone": userPhoneTextEditingController.text.trim(),
+      "id": userFirebase.uid,
+      "blockStatus": "no",
+    };
+    usersRef.set(userDataMap);
+
+    Navigator.push(context, MaterialPageRoute(builder: (c)=> const HomePage()));
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+
+              Image.asset(
+                "assets/images/logo.png"
+              ),
+
+
+
+              const Text(
+                "create a User's Account",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              //Text Fields + Button
+              Padding(
+                padding: const EdgeInsets.all(22),
+                child: Column(
+                  children: [
+
+                    TextField(
+                     controller: userNameTextEditingController,
+                     keyboardType: TextInputType.text,
+                     decoration: const InputDecoration(
+                       labelText: "User Name",
+                       labelStyle: TextStyle(
+                         fontSize: 14,
+                       ),
+                     ),
+                     style: const TextStyle(
+                       color: Colors.grey,
+                       fontSize: 15,
+                     ),
+                   ),
+
+                    const SizedBox(height: 22,),
+
+                    TextField(
+                      controller: userPhoneTextEditingController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Phone Number",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: "User Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 32,),
+
+                    ElevatedButton(
+                        onPressed: ()
+                        {
+                          checkIfNetworkIsAvailable();
+                        },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10)
+                      ),
+                      child: const Text(
+                        "Sign Up"
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 12,),
+
+              //text button
+              TextButton(
+                onPressed: ()
+                {
+                  Navigator.push(context, MaterialPageRoute(builder: (c)=> const loginScreen()));
+                },
+                child: const Text(
+                  "Already have an account? Login Here",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ), // Padding
+      ), // SingleChildScrollView
+    );
+  }
+}
+    
---
+++ lib/global/global_var.dart
@@ -0,0 +1,10 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+String userName = "";
+
+String googleMapKey = "AIzaSyBpyxJBI4AvouDL116YMg8dqvj7pFgXvtA";
+
+const CameraPosition GooglePlexInitialPosition = CameraPosition(
+  target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+);
